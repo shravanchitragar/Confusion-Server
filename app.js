875b580de039032abecea4755b801c9e72f6fcb4
Shravan Chitragar <shravan.chitragar@gmail.com>
2017-12-10 03:11:05 -0500

    Session based authentication

--- app.js
+++ app.js
@@ -52,43 +52,25 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use('/', index);
+app.use('/users', users);
 
 function auth(req,res,next){
   console.log(req.session);
 
   if(!req.session.user){
-    var authHeader = req.headers.authorization;
-    
-      if(!authHeader){
         var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');
-        err.status = 401;
+        err.status = 403;
         return next(err);
       }
     
-      var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-      var username = auth[0];
-      var password = auth[1];
-    
-      if(username=== 'admin' && password=== 'password'){
-        res.session.user = 'admin';
-        next();
-      }
-      else{
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');
-        err.status = 401;
-        return next(err);
-      }
-    
-  }
   else{
-    if(req.session.user===admin){
+    if(req.session.user=== 'authenticated'){
       next();
     }
     else{
       var err = new Error('You are not authenticated!');
-      err.status = 401;
+      err.status = 403;
       return next(err);
 
     }
@@ -101,8 +83,7 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+
 
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
